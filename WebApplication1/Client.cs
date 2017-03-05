c932ec43cc165d8113c73bdb9c385147d790ef2d
Omar Zikri <Omar Zikri>
2017-03-06 01:19:42 +1100

    initial commit

---
+++ WebApplication1/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class Client
+    {        
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Status { get; set; }
+        public string Logo { get; set; }
+        public String Address { get; set; }
+    }
+}
---
+++ WebApplication1/Scripts/Script.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="angular.min.js" />
+var myApp = angular.module("myModule", []);
+
+var myController = function ($scope, $http) {
+    $http({
+        method: 'GET',
+        url: 'http://api.demo.muulla.com/cms/merchant/all/active/10/1',       
+        headers: { Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI1NGQxOTY4MGI1MWMxNTI2MGI5NDRmZDUiLCJpc3N1ZV9kYXRlIjoiMjAxNS0wOS0wOVQwNToxMzo1My40NThaIn0.Hk2XypA_KMUnIKdSVYnwq3Rn3QyMNSQ-e80-sZsA9bY' }
+    }).then(function (response) {
+        $scope.clients = response.data;
+    });
+};
+myApp.controller("myController", myController);
